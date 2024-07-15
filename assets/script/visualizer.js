5e9e7a18cff7942df8c920491406cfc9cecd53ab
sofiasales02 <sofiasaleswk@gmail.com>
2024-07-15 19:06:12 -0300

    metodo de exclusao de gato no banco de dados

--- assets/script/visualizer.js
+++ assets/script/visualizer.js
@@ -22,6 +22,18 @@ document.addEventListener('DOMContentLoaded', () => {
             genero.textContent = cat.gender;
             linha.appendChild(genero);
 
+            const btnExcluir = document.createElement('button');
+
+            btnExcluir.addEventListener('click', () => {
+                excluirGatuxo(cat.id);
+            });
+
+            let lixImg = document.createElement('img')
+            lixImg.src = '../../images/trash-fill.svg'
+            lixImg.id = 'lixinho'
+            btnExcluir.append(lixImg);
+            linha.appendChild(btnExcluir)
+
             tabelaResposta.appendChild(linha);
 
         });
@@ -32,3 +44,22 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+// metodo para excluir o gato do banco de dados
+function excluirGatuxo (id) {
+    // metodo fetch para excluir
+    const endpoint = `http://localhost:8080/cats/${id}`
+    fetch(endpoint, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        // console.log(response.status)
+        location.reload()
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+}
